99569c7324f7b6daebcbe9d3054e8749431dc561
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-27 13:49:37 +0530

    class practice 2

---
+++ ClassPractice/Classpractice2.py
@@ -0,0 +1,6 @@
+salary= float(input("enter your current salary: "))
+YearOfService= float(input("enter your Year of service: "))
+
+if YearOfService> 5:
+    salary=salary + 1000
+print("Your net bonus amount is: ",salary,"RS")
