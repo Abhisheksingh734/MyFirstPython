e494199b8e6a160b5ee11b8d245b041467b24301
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-27 13:52:15 +0530

    array

---
+++ array.py
@@ -0,0 +1,10 @@
+a= "Hello World"
+# print(a[6])
+# print(a[0:5])
+# print(len(a))
+# if "c" in a:
+#     print("yes it is there")
+# else:
+#     print("No it is not there")
+print(a[:])
+print(a[-5:-2])
