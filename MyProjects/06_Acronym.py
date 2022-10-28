d7e855b42960e515bf86de4e9723ead7f659c687
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-28 23:55:07 +0530

    Acronyms

---
+++ MyProjects/06_Acronym.py
@@ -0,0 +1,13 @@
+a = input("enter...").split()
+
+list=[]
+for i in range(len(a)):
+    list.append(a[i][0])
+
+for i in list:
+    print(i.upper(),end="")
+
+
+    ####INCOMPLETE ############
+
+    # CANT UNDERSTAND QUESTION   # CANT UNDERSTAND QUESTION 
