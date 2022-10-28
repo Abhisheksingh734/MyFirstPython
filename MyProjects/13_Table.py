5f15ec3e80601e1e790af6358ac526f4f5999686
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-28 23:58:03 +0530

    Table project

---
+++ MyProjects/13_Table.py
@@ -0,0 +1,17 @@
+# class Employee:
+#     company="Google"
+
+# abhishek=Employee()
+# print(abhishek.company)
+# Employee.company="youtube"
+# print(abhishek.company)
+
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end="")
+    for j in range(i,n):
+        print("*",end="")
+    for j in range(i+1,n):
+        print("*",end="")
+    print()
