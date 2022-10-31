98fb0b4559c64f8d6babae56abddc5f276e5e3a6
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-31 23:27:24 +0530

    Else if statement

---
+++ ClassProject/elseifStatements.py
@@ -0,0 +1,12 @@
+MarksObtained = int(input("Enter your marks out of 100: "))
+
+if MarksObtained >90 and MarksObtained<=100:
+    print("Your Grade is A")
+elif MarksObtained > 100:
+    print("Enter your marks out of 100")
+elif MarksObtained <= 90 and MarksObtained > 80:
+    print("Your Grade is B")
+elif MarksObtained <=80 and MarksObtained> 70:
+    print("Your grade is C")
+else:
+    print("Your grade is D")
