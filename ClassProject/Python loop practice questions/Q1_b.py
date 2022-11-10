b5f4ee8d9e26ff970fb36725ac11ed41af429a12
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-11-11 00:21:14 +0530

    python loop practice question number 1

---
+++ ClassProject/Python loop practice questions/Q1_b.py
@@ -0,0 +1,10 @@
+# print   5  4  3  2  1 
+#         4  3  2  1
+#         3  2  1
+#         2  1
+#         1
+n=int(input("enter: "))
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
