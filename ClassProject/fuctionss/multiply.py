5609a1e27cc3f039b5ea5d1599db983533e8fa1c
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-11-10 19:28:19 +0530

    multiply function

---
+++ ClassProject/fuctionss/multiply.py
@@ -0,0 +1,14 @@
+
+# def multiply(num1,num2):
+#     mul=num1*num2
+#     return mul
+
+# a=multiply(2,3)
+# print(a,".2f")
+
+
+def name(n,*firstName):
+    print("My name is",firstName)
+    print(n)
+
+name("abhishek","Ankit","shubham")
