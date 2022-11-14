f729363044feffa54048e5c2854927851265a216
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-11-14 18:33:45 +0530

    list comprehension

---
+++ ClassProject/Collections/list2.py
@@ -0,0 +1,14 @@
+# numbers=[1,2,3,4,5,2,6]
+
+# n=numbers.index(2)
+# numbers[n]=200
+# print(numbers)
+
+
+
+list1=["x","y","z"]
+list2=[1,2,3]
+
+for i in list2:
+    list2.append(i)
+print(list2)
