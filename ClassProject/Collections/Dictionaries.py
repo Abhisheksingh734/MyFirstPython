5bdfe1441dddf2dd5e53a9dd6d2bba1373c017e8
Abhishek <abhisheksinghasr734@gmail.com>
2022-12-04 22:32:18 +0530

    Dictionaries

---
+++ ClassProject/Collections/Dictionaries.py
@@ -0,0 +1,39 @@
+# myDict={
+#     "name": "Abhishek",
+#     "age" : 19,
+#     "Roll.no": 43,
+#     "percentage": 88 
+# }
+
+# l,k=zip(*myDict)
+# print(l)
+# print(k)
+
+
+# print(myDict)
+# b=list(myDict.keys())
+# print(b[0])
+# # print(type(list(b)))
+# a=myDict.get("name")
+# print(a)
+# l=myDict.items()
+# print(l)
+# myDict.update({"Roll.no": 23})
+# print(myDict)
+
+# n=input().split()
+# v=input().split()
+# print(n)
+# # n=["adf","Adf","rg"]
+# # v=["asdf","asf","sef"]
+# l=zip(n,v)
+# print(dict(l))
+
+
+# myDictionary={
+#     "class":{"student":{"name":"abc","marks":{"python":90,"web":95}}}
+# }
+
+# print(myDictionary["class"]["student"]["marks"]["web"])
+
+
