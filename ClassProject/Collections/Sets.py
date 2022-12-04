ded2642956cebc5b12340d79910ff4e8e14373ab
Abhishek <abhisheksinghasr734@gmail.com>
2022-12-04 22:32:56 +0530

    Sets

---
+++ ClassProject/Collections/Sets.py
@@ -0,0 +1,40 @@
+my_set={"car","boat","train"}
+my_set2={1,2,3,4}
+my_set.update(my_set2)
+# my_set2.add("34")
+# # # my_set.pop()
+# # m=set()
+# print(my_set2)
+# print(my_set)
+# # #immutable/ unchangable
+# # print(type(m))
+
+
+# -----------------------------------------------
+
+
+# #set - store multiple values in single variable
+# #unordered
+
+# mySet1 = {"car", "boat", "train"}
+# mySet2 = {1,2,3,4}
+# mySet3 = {4,5,6,7}
+
+# # output = mySet2.union(mySet3)
+# output = mySet2.intersection(mySet3)
+# output1 = mySet2.symmetric_difference(mySet3)
+# # mySet.add("cycle")
+# # mySet1.update(mySet2)
+ 
+# print(output1)
+
+
+
+# #unchangable / immutable
+# #duplicates not allowed
+# #non homogeneous
+
+
+
+
+
