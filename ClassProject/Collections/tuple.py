3915e77870ddb605bdf585358dc7aca5cbedfdc8
Abhishek <abhisheksinghasr734@gmail.com>
2022-12-06 09:35:45 +0530

    tuples

---
+++ ClassProject/Collections/tuple.py
@@ -0,0 +1,28 @@
+# t=tuple(map(int,input().split()))
+# print(t)
+#                 -----------------------------tuple packing-----------------------------------
+
+# tuple1=("car",)
+# (item,*item2)=tuple1
+# print(item)
+# print(item2)
+                #    --------------------------------tuple UNPACKING---------------------------------
+
+# tuple1=(10,20,30,40)
+# a,b,c,d=tuple1
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+
+# count=0
+# while count<len(tuple1):
+#     print(tuple1[count])
+#     count+=1
+
+t=tuple(input().split())
+print(t)
+
+
